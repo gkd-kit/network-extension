0daff8a2be03b0cbc3919af2f022a24d0e15f9c9
lisonge <i@songe.li>
2023-05-06 17:25:07 +0800

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { unsafeWindow, GM_xmlhttpRequest } from '$';
+import { GM_fetch } from 'monkey-extra'
+
+const GmNetworkExtension = {
+    GM_xmlhttpRequest,
+    GM_fetch
+}
+
+Object.assign(unsafeWindow, { GmNetworkExtension })
+
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import monkey from 'vite-plugin-monkey';
+
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      monkey({
+        entry: 'src/main.ts',
+        userscript: {
+          icon: 'https://vitejs.dev/logo.svg',
+          namespace: 'https://github.com/lisonge',
+          author:`lisonge`,
+          description: {
+            '': `network-extension`,
+          },
+          match: command == 'serve' ? [`https://i.songe.li/*`] : [
+            `http://*/*`, `https://*/*`,
+          ],
+        },
+      }),
+    ],
+  }
+});
