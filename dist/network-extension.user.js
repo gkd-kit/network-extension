d7be3f0d5f390ddd22928f521208e1cda682c13c
lisonge <i@songe.li>
2023-05-15 11:45:30 +0800

    feat: default value

--- dist/network-extension.user.js
+++ dist/network-extension.user.js
@@ -149,10 +149,16 @@
     register();
     return controller;
   };
+  const defaultValue = [
+    `gkd-viewer.songe.li`,
+    `gkd-viewer.vercel.app`,
+    `gkd-viewer.netlify.app`,
+    `gkd-viewer.gitee.io`
+  ].includes(location.hostname);
   const storeKey = `inject_network:` + location.origin;
   const menu = useCheckedMenu(
     `inject api to current website`,
-    _GM_getValue(storeKey, false)
+    _GM_getValue(storeKey, defaultValue)
   );
   menu.onChange = (checked) => {
     menu.checked = checked;
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { GM_getValue, GM_setValue, GM_xmlhttpRequest, unsafeWindow } from '$';
 import { GM_fetch } from 'monkey-extra';
 import { useCheckedMenu } from './utils';
 
+const defaultValue = [
+  `gkd-viewer.songe.li`,
+  `gkd-viewer.vercel.app`,
+  `gkd-viewer.netlify.app`,
+  `gkd-viewer.gitee.io`,
+].includes(location.hostname);
+
 const storeKey = `inject_network:` + location.origin;
 const menu = useCheckedMenu(
   `inject api to current website`,
-  GM_getValue(storeKey, false),
+  GM_getValue(storeKey, defaultValue),
 );
+
 menu.onChange = (checked) => {
   menu.checked = checked;
   GM_setValue(storeKey, checked);
